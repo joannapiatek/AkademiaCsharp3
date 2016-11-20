3872b1ef416a840b5095a0dea50345c933393164
Joanna <fickle.joan@gmail.com>
2016-11-21 00:11:00 +0100

    Final version

---
+++ AkademiaCSharp3/Politician.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkademiaCSharp3
+{
+    public class Politician : Citizen
+    {
+        public Politician(string name, double income)
+        {
+            Name = name;
+            Income = income;
+            TaxRate = 0.34;
+        }
+
+        public override double CalculateTax()
+        {
+            return Income*TaxRate + 2000;
+        }
+    }
+}
